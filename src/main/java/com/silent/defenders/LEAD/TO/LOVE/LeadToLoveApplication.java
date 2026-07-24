567acac1ed32e11d68463866242ee5e2d19efc3d
MOHAMMAD ABUZAR <mdabuzarsaad@gmail.com>
2026-07-24 10:18:10 +0530

    added files

---
+++ src/main/java/com/silent/defenders/LEAD/TO/LOVE/LeadToLoveApplication.java
@@ -0,0 +1,13 @@
+package com.silent.defenders.LEAD.TO.LOVE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadToLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadToLoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silent/defenders/LEAD/TO/LOVE/LeadToLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silent.defenders.LEAD.TO.LOVE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadToLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
